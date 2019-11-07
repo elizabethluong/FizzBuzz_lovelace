ecd8a9f1666cfe93b3171acb38a975c05b9e3556
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-07 13:34:21 +0000

    Update FizzBuzz.js
    Editing the FizzBuzz function to make it look nice.

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,13 +1,16 @@
 function FizzBuzz(number) {
-   // Return FizzBuzz if the number is divisible by 3 & 5
+    // Return FizzBuzz if the number is divisible by 3 & 5
     if (number % 3 === 0 && number % 5 === 0) {
         return "FizzBuzz";
     }
+    // Return Fizz if the number is divisible by 3 
     else if (number % 3 === 0 ) {
-        return "fizz"}
+        return "Fizz"
+    }
+    // Return Buzz if the number is divisible by 5
     else if (number % 5 === 0) {
-    return 'buzz';
-  }
-
-return number
+        return 'Buzz';
+    }
+    
+    return number
 }
