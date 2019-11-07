7e0320e3e4f459639af97857903eb38002dfa5c8
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-07 12:53:48 +0000

    FizzBuzz if-statement

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,5 +1,6 @@
 function FizzBuzz(number) {
-
+    // Return FizzBuzz if the number is divisible by 3 & 5
+    if (number % 3 === 0 && number % 5 === 0) {
+        return "FizzBuzz";
+    }
 }
-
-console.log();
